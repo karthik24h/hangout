7432d867628e0c3d770c726ad23393c2ba9d33a6
karthik <karthikreddie08@gmail.com>
2025-05-01 23:43:09 +0530

    feat : addded audio lib dependency

--- frontend/src/MusicPage.jsx
+++ frontend/src/MusicPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 export default function MusicPage() {
   return (
@@ -9,6 +11,12 @@ export default function MusicPage() {
       <div className="content p-4">
         <h2>🎵 Music</h2>
         <p>Listen to music together with your friends!</p>
+        <AudioPlayer
+          autoPlay
+          src="http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3"
+          onPlay={e => console.log("onPlay")}
+          // other props here
+        />
       </div>
     </div>
   );
